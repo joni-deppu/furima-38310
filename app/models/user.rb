39a35f9abb6881b8837efd1ce0a05f1e2b9d3c76
joni-deppu <44shuxichuan@gmail.com>
2022-08-18 01:10:02 +0900

    文字制限

--- app/models/user.rb
+++ app/models/user.rb
@@ -5,11 +5,20 @@ class User < ApplicationRecord
          :recoverable, :rememberable, :validatable
 
   validates :nickname, presence: true
-  validates :first_name_zenkaku, presence: true
-  validates :last_name_zenkaku, presence: true
-  validates :first_name_katakana, presence: true
-  validates :last_name_katakana, presence: true
-  validates :birthday, presence: true
+  with_options presence: true, format: { with: /\A[ぁ-んァ-ヶ一-龥々ー]+\z/, message: '全角（漢字・ひらがな・カタカナ）を使用してください' } do
+    validates :first_name_zenkaku, presence: true
+    validates :last_name_zenkaku, presence: true
+  end
+
+  with_options presence: true, format: { with: /\A[ァ-ヶー]+\z/, message: '全角（カタカナ）を使用してください' } do
+    validates :first_name_katakana, presence: true
+    validates :last_name_katakana, presence: true
+  end
+
+    validates :birthday, presence: true
+
+  PASSWORD_REGEX = /\A(?=.*?[a-z])(?=.*?[\d])[a-z\d]+\z/i.freeze
+  validates_format_of :password, with: PASSWORD_REGEX, message: 'には英字と数字の両方を含めて設定してください'
 
   has_many :items
   has_many :buys
