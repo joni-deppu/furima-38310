90d7666d2c6317e578443f46d0a70be404d2e023
joni-deppu <44shuxichuan@gmail.com>
2022-08-18 17:11:30 +0900

    単体テストコード実装

--- app/models/user.rb
+++ app/models/user.rb
@@ -5,12 +5,12 @@ class User < ApplicationRecord
          :recoverable, :rememberable, :validatable
 
   validates :nickname, presence: true
-  with_options presence: true, format: { with: /\A[ぁ-んァ-ヶ一-龥々ー]+\z/, message: '全角（漢字・ひらがな・カタカナ）を使用してください' } do
+  with_options presence: true, format: { with: /\A[ぁ-んァ-ヶ一-龥々ー]+\z/, message: 'is invalid. Input full-width characters' } do
     validates :first_name_zenkaku, presence: true
     validates :last_name_zenkaku, presence: true
   end
 
-  with_options presence: true, format: { with: /\A[ァ-ヶー]+\z/, message: '全角（カタカナ）を使用してください' } do
+  with_options presence: true, format: { with: /\A[ァ-ヶー]+\z/, message: 'is invalid. Input full-width katakana characters' } do
     validates :first_name_katakana, presence: true
     validates :last_name_katakana, presence: true
   end
@@ -18,7 +18,7 @@ class User < ApplicationRecord
     validates :birthday, presence: true
 
   PASSWORD_REGEX = /\A(?=.*?[a-z])(?=.*?[\d])[a-z\d]+\z/i.freeze
-  validates_format_of :password, with: PASSWORD_REGEX, message: 'には英字と数字の両方を含めて設定してください'
+  validates_format_of :password, with: PASSWORD_REGEX, message: 'is invalid. Include both letters and numbers'
 
   has_many :items
   has_many :buys
--- spec/factories/users.rb
+++ spec/factories/users.rb
@@ -1,10 +1,15 @@
 FactoryBot.define do
   factory :user do
-   nickname              {Faker::Name.initials(number: 2)}
+
+   nickname              {Faker::Name.name}
+   first_name_zenkaku    {"田中"}
+   last_name_zenkaku     {"信玄"}
+   first_name_katakana   {"カトウ"}
+   last_name_katakana    {"ケンシン"}
    email                 {Faker::Internet.free_email}
    password              {Faker::Internet.password(min_length: 6)}
    password_confirmation {password}
+   birthday              {Faker::Date.birthday}
+
   end
  end
-
- 項目は後で書き直す
