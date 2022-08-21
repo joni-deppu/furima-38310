9dde2828464927117b3e43a4239f158ab06a9d93
joni-deppu <44shuxichuan@gmail.com>
2022-08-22 00:23:05 +0900

    テスト項目追加

--- spec/models/item_spec.rb
+++ spec/models/item_spec.rb
@@ -67,6 +67,11 @@ RSpec.describe Item, type: :model do
         @item.valid?
         expect(@item.errors.full_messages).to include("Price must be less than or equal to 9999999")
       end
+      it 'priceが半角数値でないと登録できない' do
+          @item.price = '９９９'
+          @item.valid?
+          expect(@item.errors.full_messages).to include("Price is not a number")
+        end
       it 'userが紐付いていないと保存できない' do
         @item.user = nil
         @item.valid?
