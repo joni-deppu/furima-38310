2a0be2365acffd53e374722c96f41169845aa0bd
joni-deppu <44shuxichuan@gmail.com>
2022-08-21 18:32:18 +0900

    軽微な修正

--- app/models/item.rb
+++ app/models/item.rb
@@ -10,8 +10,9 @@ class Item < ApplicationRecord
   # has_one :buy
 
   validates :item_name, :detail, :price, :image, presence: true
-  
-
+  with_options presence: true, format: { with: /\A(\d)+\z/ } do
+    validates :price, numericality: { greater_than_or_equal_to: 300, less_than_or_equal_to: 9999999 }
+  end
  #カテゴリーの選択が「---」の時は保存できないようにする,空だめ
  validates :category_id, :condition_id, :cost_id, :prefecture_id, :scheduled_day_id, numericality: { other_than: 1 , message: "can't be blank"}, presence: true
 end
