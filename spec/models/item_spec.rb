69586cd4a14f4796ccf5411ca47a60f8f76e68b8
joni-deppu <44shuxichuan@gmail.com>
2022-08-22 21:06:20 +0900

    商品一覧表示

--- spec/models/item_spec.rb
+++ spec/models/item_spec.rb
@@ -38,12 +38,12 @@ RSpec.describe Item, type: :model do
         expect(@item.errors.full_messages).to include('Detail is too long (maximum is 1000 characters)')
       end
       it 'カテゴリーに「---」が選択されている場合は出品できない' do
-        @item.category_id = 1 
+        @item.category_id = 1
         @item.valid?
         expect(@item.errors.full_messages).to include("Category can't be blank")
       end
       it '商品の状態に「---」が選択されている場合は出品できない' do
-        @item.condition_id = 1 
+        @item.condition_id = 1
         @item.valid?
         expect(@item.errors.full_messages).to include("Condition can't be blank")
       end
