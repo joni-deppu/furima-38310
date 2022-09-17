e381d04f192527c85cb5aefb477343dba5bb25f7
joni-deppu <44shuxichuan@gmail.com>
2022-09-17 19:38:13 +0900

    tag単体テストコード

--- spec/models/tag_spec.rb
+++ spec/models/tag_spec.rb
@@ -1,5 +1,29 @@
 require 'rails_helper'
 
 RSpec.describe Tag, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  before do
+    @tag = FactoryBot.build(:tag)
+  end
+
+  describe 'タグ新規登録' do
+    context '新規登録できるとき' do
+      it 'タグが存在すれば登録できる' do
+        expect(@tag).to be_valid
+      end
+    end
+    context '新規登録できないとき' do
+      it 'タグが空では登録できない' do
+        @tag.tag_name = ''
+        @tag.valid?
+        expect(@tag.errors.full_messages).to include("Tag name can't be blank")
+      end
+      it '重複したタグが存在する場合は登録できない' do
+        @tag.save
+        another_tag = FactoryBot.build(:tag)
+        another_tag.tag_name = @tag.tag_name
+        another_tag.valid?
+        expect(another_tag.errors.full_messages).to include('Tag name has already been taken')
+      end
+    end
+  end
 end
